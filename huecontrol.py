7ec0aac943bd487feb5d7c478c18db6e1886e31d
Grant MacGillivray <grant@snuggle.cat>
2016-07-24 20:26:27 -0700

    Light selection
    Allows you to select lights. So long as your light setup is exactly the
    same as mine. :P

--- huecontrol.py
+++ huecontrol.py
@@ -2,18 +2,45 @@ from qhue import Bridge, create_new_username
 username = create_new_username("10.0.0.96")
 b = Bridge("10.0.0.96", username)
 
+light = raw_input("Which lights are you looking to turn on? Valid inputs are Bedroom, Office, or All.")
+
 command = raw_input("Are you turning your lights on or off? ")
 
 
-if command.lower() == "on":
-    bright = raw_input("How bright would you like the lights to be, where 1 is the dimmest and 254 is the brightest? ")
-    if int(bright) >= 1 and int(bright) <= 254:
-        b.lights[1].state(on = True, bri = int(bright))
+if light.lower() == "office":
+    if command.lower() == "on":
+        bright = raw_input("How bright would you like the lights to be, where 1 is the dimmest and 254 is the brightest? ")
+        if int(bright) >= 1 and int(bright) <= 254:
+            b.lights[1].state(on = True, bri = int(bright))
+    elif command.lower() == "off":
+        b.lights[1].state(on = False)
     else:
         print "You must enter a number between 1 and 254."
 
-elif command.lower() == "off":
-    b.lights[1].state(on = False)
+elif light.lower() == "bedroom":
+    if command.lower() == "on":
+        bright = raw_input("How bright would you like the lights to be, where 1 is the dimmest and 254 is the brightest? ")
+        if int(bright) >= 1 and int(bright) <= 254:
+            b.lights[2].state(on = True, bri = int(bright))
+    elif command.lower() == "off":
+        b.lights[2].state(on = False)
+    else:
+        print "You must enter a number between 1 and 254."
+
+elif light.lower() == "all":
+    if command.lower() == "on":
+        bright = raw_input("How bright would you like the office light to be, where 1 is the dimmest and 254 is the brightest? ")
+        if int(bright) >= 1 and int(bright) <= 254:
+            b.lights[1].state(on = True, bri = int(bright))
+        bright = raw_input("How bright would you like the office light to be, where 1 is the dimmest and 254 is the brightest? ")
+        if int(bright) >= 1 and int(bright) <= 254:
+            b.lights[2].state(on = True, bri = int(bright))
+    elif command.lower() == "off":
+        b.lights[1,2].state(on = False)
+    else:
+        print "You must enter a number between 1 and 254."
+
+
 
 else:
-    print "You have to type either on or off."
+    print "You must select a valid input."
